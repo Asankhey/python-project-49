64ef27f3f5eb61bc1022c764aff0ed9395dc25a4
asankheya <lilitasa@proton.me>
2024-07-29 16:37:19 +0300

    step7

---
+++ brain_games/scripts/brain_gcd.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+import prompt
+from random import randint
+import math
+
+
+def main():
+    print("Welcome to the Brain Games!")
+    user_name = prompt.string('May I have your name? ')
+    print(f"Hello {user_name}!\nFind the greatest common divisor of given numbers.")
+    index = 0
+    score = 3
+    while index < score:
+        num1 = randint(1, 20)
+        num2 = randint(1, 20)
+        gcd = math.gcd(num1, num2)
+        print(f'Question: {num1} {num2}')
+        answer = prompt.string('Your answer: ')
+        if int(answer) == gcd:
+            print('Correct!')
+            index += 1
+        else:
+            print(f"'{answer}' is wrong answer ;(. Correct answer was {gcd}")
+            return
+    print(f"Congratulations, {user_name}!")
+
+
+if __name__ == '__main__':
+    main()
