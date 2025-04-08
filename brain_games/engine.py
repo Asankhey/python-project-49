fdddaac11fa98d01713c58e71a383ce0d9677a32
asankheya <lilitasa@proton.me>
2025-04-08 22:54:15 +0700

    Исправлена длина строки в engine.py

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -16,7 +16,8 @@ def run_game(game):
         if answer == correct_answer:
             print("Correct!")
         else:
-            print(f"'{answer}' is wrong answer ;(. Correct answer was '{correct_answer}'.")
+            print(f"'{answer}' is wrong answer ;(.")
+            print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {user_name}!")
             return
 
